6f69a408aed13e753b9018849bef9af140a3ffb0
martin admin <actserv@actserv-hp-nb-15-two.actserv.local>
2021-02-16 17:27:58 +0300

    scrapper for job listing

---
+++ scrappers/job_listing_scrappers.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.monster.com/jobs/search/?q=Real-estate-agent&where=Munich'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id='ResultsContainer')
+
+job_elems = results.find_all('section', class_='card-content')
+
+for job_elem in job_elems:
+    print(job_elem, end='\n'*2)
+
+
+
+for job_elem in job_elems:
+    # Each job_elem is a new BeautifulSoup object.
+    # You can use the same methods on it as you did before.
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    print(title_elem)
+    print(company_elem)
+    print(location_elem)
+    print()
+
+
+
+for job_elem in job_elems:
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    print(title_elem.text)
+    print(company_elem.text)
+    print(location_elem.text)
+    print()
+
+
+
+for job_elem in job_elems:
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    if None in (title_elem, company_elem, location_elem):
+        continue
+    print(title_elem.text.strip())
+    print(company_elem.text.strip())
+    print(location_elem.text.strip())
+    print()
+
+
+real_estate_agent_jobs = results.find_all('h2', string='Real Estate Agent')
+
+real_estate_jobs = results.find_all('h2',string=lambda text: 'real_estate' in text.lower())
+
+for real_estate_job in real_estate_jobs:
+    link = real_esatate_job.find('a')['href']
+    print(real_estate_job.text.strip())
+    print(f"Apply here: {link}\n")
